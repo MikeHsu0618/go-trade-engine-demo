48f7922de483d1386f71d4cf3610f9be350b4165
mike <a3698521tw11@gmail.com>
2022-09-18 00:57:46 +0800

    style: separate queue struct

---
+++ internal/trade/entity/queue/priority_queue.go
@@ -0,0 +1,30 @@
+package queue
+
+type PriorityQueue []QueueItem
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Less(pq[j])
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].SetIndex(i)
+	pq[j].SetIndex(j)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.SetIndex(-1)
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	x.(QueueItem).SetIndex(n)
+	*pq = append(*pq, x.(QueueItem))
+}
